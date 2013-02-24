06c00324d53f0a410042415ff4811a775126d397
Jen de Gruchy <jendegruchy@Jens-MacBook-Pro.local>
2013-02-24 10:22:04 -0500

    Initial Commit

---
+++ Hello-World/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello-World
+//
+//  Created by Jen de Gruchy on 2/24/13.
+//  Copyright (c) 2013 Jen de Gruchy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
